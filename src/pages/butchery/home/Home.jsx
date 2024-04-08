dc2b7b49a1d4a6ebcd83e388879ba3e07ec51699
admin <admin@admins-MacBook-Air.local>
2024-04-08 14:57:17 +0500

    add data ...

--- src/pages/butchery/home/Home.jsx
+++ src/pages/butchery/home/Home.jsx
@@ -1,13 +1,64 @@
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 import "./Home.css";
 import { useGetButcheryAllDataQuery } from "../../../app/butchery";
 
 const Home = () => {
-  //   const allButcheryData = useGetButcheryAllDataQuery();
+  const allButcheryData = useGetButcheryAllDataQuery();
+  const { username } = JSON.parse(sessionStorage.getItem("adminInfo"));
+  const [data, setData] = useState({
+    addMeatKg: [null],
+    addMeat: [null],
+    addMincedMeat: [null],
+  });
+  useEffect(() => {
+    let findUser = allButcheryData?.data?.innerData.find(
+      (f) => f.username === username
+    );
 
-  //   useEffect(() => {
-  //     console.log(allButcheryData.data);
-  //   }, [allButcheryData.data]);
+    const today = new Date().toISOString().substring(0, 10);
+
+    // Bugungi sana bilan mos keladigan addMeatKg obyektlarini topish
+    const addMeatKg = findUser?.addMeatKg.filter((addTime) => {
+      const itemDate = new Date(addTime.addetTime)
+        .toISOString()
+        .substring(0, 10);
+      return itemDate === today;
+    });
+
+    // Bugungi sana bilan mos keladigan addMeat obyektlarini topish
+    const addMeat = findUser?.addMeat.filter((addTime) => {
+      const itemDate = new Date(addTime.addetTime)
+        .toISOString()
+        .substring(0, 10);
+      return itemDate === today;
+    });
+
+    // Bugungi sana bilan mos keladigan addMincedMeat obyektlarini topish
+    const addMincedMeat = findUser?.addMincedMeat.filter((addTime) => {
+      const itemDate = new Date(addTime.addetTime)
+        .toISOString()
+        .substring(0, 10);
+      return itemDate === today;
+    });
+
+    // console.log(
+    //   "addMeatKg:",
+    //   addMeatKg?.map((q) => q?.meatKg?.quantity),
+    //   "addMeat",
+    //   addMeat?.map((q) => q?.meat?.quantity),
+    //   "addMincedMeat:",
+    //   addMincedMeat?.map((q) => q?.mincedMeat?.quantity)
+    // );
+
+    setData({
+      addMeat: addMeat?.map((q) => q?.meat?.quantity),
+      addMeatKg: addMeatKg?.map((q) => q?.meatKg?.quantity),
+      addMincedMeat: addMincedMeat?.map((q) => q?.mincedMeat?.quantity),
+    });
+  }, [allButcheryData?.data]);
+
+  console.log(data);
+
   return (
     <div className="home_brutchery">
       <div className="bruchery_header_home">
@@ -19,8 +70,13 @@ const Home = () => {
         <ul className="brutchery_meat_data_ammount">
           <li className="text">Soni:</li>
           <li className="ammount">
-            <span>000</span>
-            <span>000</span>
+            {data?.addMeat?.length > 0 ? (
+              data?.addMeat?.map((item, index) => (
+                <span key={index}>{item + " dona"}</span>
+              ))
+            ) : (
+              <span>Bugun malumot qoshilmadi</span>
+            )}
           </li>
         </ul>
         <div className="brutchery_meat_full_data">
@@ -31,8 +87,13 @@ const Home = () => {
         <ul className="brutchery_meat_data_ammount">
           <li className="text">Soni:</li>
           <li className="ammount">
-            <span>000</span>
-            <span>000</span>
+            {data?.addMincedMeat?.length > 0 ? (
+              data?.addMincedMeat?.map((item, inx) => (
+                <span key={inx}>{item + " dona"}</span>
+              ))
+            ) : (
+              <span>Bugun malumot qoshilmadi</span>
+            )}
           </li>
         </ul>
         <div className="brutchery_meat_full_data">
@@ -43,8 +104,13 @@ const Home = () => {
         <ul className="brutchery_meat_data_ammount">
           <li className="text">Soni:</li>
           <li className="ammount">
-            <span>000</span>
-            <span>000</span>
+            {data?.addMeatKg?.length > 0 ? (
+              data?.addMeatKg?.map((item, inx) => (
+                <span key={inx}>{item + " kg"}</span>
+              ))
+            ) : (
+              <span>Bugun malumot qoshilmadi</span>
+            )}
           </li>
         </ul>
         <div className="brutchery_meat_full_data">
